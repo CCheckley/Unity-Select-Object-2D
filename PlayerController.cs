69a7ac97f97ab5cf7dfbf82a2baaa4cde4e4498c
unknown <connor.checkley@gmail.com>
2019-12-03 16:35:27 +0000

    Initial commit

---
+++ PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        MouseInput();
+    }
+
+    void MouseInput()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 raycastPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(raycastPos, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            }
+        }
+    }
+}
